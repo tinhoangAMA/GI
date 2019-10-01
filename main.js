4f024541f07da2d0ad6f676c273fcc21a2cab769
Tin Hoang <tin.hoang@ama.ab.ca>
2019-10-01 14:26:48 -0600

    changed value of Click me so it will pass!

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<html><body><input id="btn" type="button" value="Click me 1!"></body></html>\n');
+    res.end('<html><body><input id="btn" type="button" value="Click me!"></body></html>\n');
 }).listen(3001, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:3001/'); 
