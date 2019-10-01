b20f360f36d3893027608497b884e0f1da4208e6
Tin Hoang <tin.hoang@ama.ab.ca>
2019-10-01 11:40:22 -0600

    fixed call to ngrok

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('<html><body><input id="btn" type="button" value="Click me!"></body></html>\n');
-}).listen(8081, '127.0.0.1');
+}).listen(8082, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:8082/');
